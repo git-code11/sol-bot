3f42c81eb0598be7b7f62b14c9a48767f7409bf8
Victor <vector.code11@gmail.com>
2024-12-16 03:23:24 +0000

    update

--- meme_bot/main.py
+++ meme_bot/main.py
@@ -9,27 +9,33 @@ def start():
         inquirer.Text("target_addr", 
                       message="Enter target address", 
                       validate=non_empty_validator,
-                      default="87esLg2WdmndkRjYrxNene4cXrGsSAeVyQxAG9tcRh1m"),
+                    #  default="87esLg2WdmndkRjYrxNene4cXrGsSAeVyQxAG9tcRh1m"
+                    ),
         inquirer.Text("amount", 
                       message="Enter amount in sol to purchase", 
                       validate=non_empty_validator, 
-                      default=0.001),
+                    #  default=0.001
+                    ),
         inquirer.Text("timeout", 
                       message="Enter the sale time interval from sales in seconds", 
                       validate=non_empty_validator, 
-                      default=30),
+                    #  default=30
+                    ),
         inquirer.Text("min_profit", 
                       message="Enter the profit amount for sales in dollars", 
                       validate=non_empty_validator, 
-                      default=0.00001),
+                    #  default=0.00001
+                    ),
         inquirer.Text("poll_interval", 
                       message="Enter Polling Interval in seconds", 
                       validate=non_empty_validator, 
-                      default=1*60),
+                    #  default=1*60
+                    ),
         inquirer.Text("bot_secret", 
                       message="Enter Bot SecretKey", 
                       validate=non_empty_validator,
-                      default="8e41ad5b7a6c5e9a7cee9d3b46a9c34bf2ac5efeb1fcfc9b62522c0d862ceb24"),
+                    #  default="8e41ad5b7a6c5e9a7cee9d3b46a9c34bf2ac5efeb1fcfc9b62522c0d862ceb24"
+                        ),
         inquirer.Checkbox(
             "sale_type",
             message="Select the meme coin sales strategy?",
@@ -43,7 +49,7 @@ def start():
     ]
 
     print_screen()
-          
+
     data = inquirer.prompt(questions)
     # preprocess
     data['amount'] = utils.sol_to_lamport(float(data['amount']))
